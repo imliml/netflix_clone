ec6192f255acd73da9fa6354f228c172de1f0a8c
lsr <imlimr1232@gmail.com>
2020-08-22 16:13:43 +0900

    css in header #2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Router from "./Components/Router";
+import GlobalStyles from "./Components/GlobalStyles";
 
 import "./App.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Router />
+      <GlobalStyles />
     </>
   );
 }
