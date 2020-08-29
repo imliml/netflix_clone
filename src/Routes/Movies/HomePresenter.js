b162194ca9deb8af9d3ecd0cc32c0066a03fb441
lsr <imlimr1232@gmail.com>
2020-08-29 17:00:08 +0900

    section and loader setting #2

--- src/Routes/Movies/HomePresenter.js
+++ src/Routes/Movies/HomePresenter.js
@@ -16,21 +16,21 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) => {
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
           {nowPlaying.map((movie) => (
-            <span> {movie.title} </span>
+            <span key={movie.id}> {movie.title} </span>
           ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular">
           {popular.map((movie) => (
-            <span> {movie.title}</span>
+            <span key={movie.id}> {movie.title}</span>
           ))}
         </Section>
       )}
       {upcoming && upcoming.length > 0 && (
         <Section title="Upcoming">
           {upcoming.map((movie) => (
-            <span> {movie.title}</span>
+            <span key={movie.id}> {movie.title}</span>
           ))}
         </Section>
       )}
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,5 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const TVPresenter = ({
   today,
@@ -9,10 +16,39 @@ const TVPresenter = ({
   loading,
   error,
 }) => {
-  return (
-    <div>
-      <h1>{today.length}</h1>
-    </div>
+  return loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {today && today.length > 0 && (
+        <Section title="Today">
+          {today.map((tv) => (
+            <span key={tv.id}> {tv.name} </span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular">
+          {popular.map((tv) => (
+            <span key={tv.id}>{tv.name}</span>
+          ))}
+        </Section>
+      )}
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated">
+          {topRated.map((tv) => (
+            <span key={tv.id}> {tv.name} </span>
+          ))}
+        </Section>
+      )}
+      {thisWeek && thisWeek.length > 0 && (
+        <Section title="This Week">
+          {thisWeek.map((tv) => (
+            <span key={tv.id}> {tv.name} </span>
+          ))}
+        </Section>
+      )}
+    </Container>
   );
 };
 
