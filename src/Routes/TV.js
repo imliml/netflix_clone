111efbaf7435eda81e1dd0e2d6f15a1e395c5ed6
lsr <imlimr1232@gmail.com>
2020-08-29 15:22:07 +0900

    네트워킹 #2

--- src/Routes/TV.js
+++ src/Routes/TV.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { tvApi } from "../api";
 
 const TV = () => {
+  // 상태값 선언
+  const [tv, setTv] = useState({
+    results: [],
+    error: null,
+  });
+
+  // 데이터 불러오는 함수
+  const getData = async () => {
+    const [results, error] = await tvApi.popular();
+    setTv({
+      results,
+      error,
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div>
-      <h1>TV</h1>
+      <h1>{tv.results.length}</h1>
     </div>
   );
 };
